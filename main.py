8161cfa89c7dea9aae3cc926fc33f8b6d539f15d
Alexander <a9169590391@gmail.com>
2021-11-05 16:57:27 +0300

    commit1

--- main.py
+++ main.py
@@ -2,25 +2,33 @@ import sys
 import sqlite3
 from AppTimeUI import Ui_AppTime
 from LimitsUI import Ui_LimitsDialog
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
 
 
-class LimitsDialog(QMainWindow, Ui_LimitsDialog):
-    def __init__(self):
-        super().__init__()
+class LimitsDialog(QDialog, Ui_LimitsDialog):
+    def __init__(self, parent=None):
+        self._parent = parent
+        super().__init__(parent)
         self.setupUi(self)
+        self.okLimitsButton.clicked.connect(self.ok_pressed)
+
+    def ok_pressed(self):
+        self.close()
+
+    def closeEvent(self, a):
+        self._parent.show()
 
 
 class MainWindow(QMainWindow, Ui_AppTime):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.limitsButton.clicked.connect(self.showlimitsdialog)
+        self.limitsButton.clicked.connect(self.show_limits_dialog)
 
-    def showlimitsdialog(self):
-        dialog_window = LimitsDialog()
-        dialog_window.show()
+    def show_limits_dialog(self):
         self.hide()
+        dialog_window = LimitsDialog(self)
+        dialog_window.exec_()
 
 
 if __name__ == '__main__':
